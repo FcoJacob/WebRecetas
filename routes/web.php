21da11e006f8bb9fa8e42df3de007ff0400e070f
FcoJacob <FcoJacob@users.noreply.github.com>
2018-02-15 16:05:40 +0000

    Config Route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('recipes', 'RecipesController@index',['except' => ['create', 'show', 'edit']]);
